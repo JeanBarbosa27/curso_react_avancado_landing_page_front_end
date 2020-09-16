9b1061ab806dfa2590bfa3c9d18ba6a111260cf3
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-09-16 13:49:42 -0300

    Add section about project query

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,35 +1,46 @@
 const GET_LANDING_PAGE = /* GraphQL*/ `
-fragment imageData on UploadFile{
-  alternativeText
-  url
-}
+  fragment imageData on UploadFile{
+    alternativeText
+    url
+  }
 
-fragment logo on LandingPage {
-  logo {
-     ...imageData
-    }
-}
-
-fragment header on LandingPage {
-  header {
-    title
-    description
-    button {
-      label
-      url
-    }
-    image {
+  fragment logo on LandingPage {
+    logo {
       ...imageData
+      }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
+        ...imageData
+      }
     }
   }
-}
 
-query GET_LANDING_PAGE {
-  landingPage {
-    ...logo
-    ...header
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+        image {
+          ...imageData
+        }
+        title
+        description
+      }
   }
-}
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+      ...sectionAboutProject
+    }
+  }
 `
 
 export default GET_LANDING_PAGE
